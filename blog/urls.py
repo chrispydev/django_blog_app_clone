89ac7a3610bfc2d36c3162f4d1a3581fef039254
chrispydev <christianowusu44@gmail.com>
2020-08-12 16:30:15 +0000

    post detail function

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from blog import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
+    path('post-create', views.PostCreateView.as_view(), name='post_create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from blog.models import Post
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 
 
 class PostListView(ListView):
@@ -11,3 +12,14 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     context_object_name = 'posts'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    context_object_name = 'post'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
